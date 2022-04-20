643565ecf0b02707a226d10eafea9497448fd29f
Evgeny Timofeev <repeatitplease@gmail.com>
2022-04-20 14:40:45 +0300

    Собрана визуальная модель приложения

---
+++ Armageddon 2022/AsteroidInfoCell.swift
@@ -0,0 +1,19 @@
+//
+//  AsteroidInfoCell.swift
+//  Armageddon 2022
+//
+//  Created by Евгений Тимофеев on 20.04.2022.
+//
+
+import UIKit
+
+class AsteroidInfoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
+    
+
---
+++ Armageddon 2022/Controllers/AsteriodInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AsteriodInfoViewController.swift
+//  Armageddon 2022
+//
+//  Created by Евгений Тимофеев on 20.04.2022.
+//
+
+import UIKit
+
+class AsteriodInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
